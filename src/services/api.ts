e2924d1b6ec0bbe322f56958e1737dea48ba0af1
mateush-souza <mateusguns798@gmail.com>
2024-11-04 21:37:01 -0300

    revert: api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,48 +1,19 @@
+import { getAPIClient, RegisterResponse, simulateRegister } from "./axios";
 
-import axios from 'axios';
+export const api = getAPIClient()
 
-const api = axios.create({
-  baseURL: 'http://localhost:8080/'
-});
+export const registerUser = async (data: { name: string; email: string; password: string }): Promise<RegisterResponse> => {
+        const response = await simulateRegister(data);
+        return response;
+};
 
-export async function loginUser(data: { email: string; password: string }) {
-  try {
-    // Ajuste aqui para usar GET com os parâmetros no URL
-    const response = await api.get('/login', {
-      params: {
-        email: data.email,
-        password: data.password,
-      },
-    });
+export default async function handler(req: any, res: any) {
+    if (req.method === 'POST') {
+      const { placa, marca, tipoMotor, transmissao, cor, quilometragem, manutencaoRecente, observacoes } = req.body;
 
-    // Supondo que a API retorna um array de usuários
-    const users = response.data; // A resposta é um array de usuários
-    const user = users.find((user: { email: string }) => user.email === data.email);
-
-    if (!user) {
-      throw new Error("Usuário não encontrado ou senha incorreta");
+      res.status(200).json({ message: 'Veículo cadastrado com sucesso', data: req.body });
+    } else {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Método ${req.method} não permitido`);
     }
-
-    // Aqui você pode gerar um token fictício ou obter um token real da sua API
-    const token = `token_for_user_${user.id}`; // Token fictício baseado no ID do usuário
-
-    return { token, email: user.email };
-  } catch (error) {
-    console.error("Erro no login:", error);
-    throw error;
   }
-}
-
-
-export async function registerUser(data: { name: string; email: string; password: string }) {
-  try {
-    const response = await api.post('usuario/registro', data);
-    return response.data;
-  } catch (error) {
-    console.error("Erro no cadastro:", error);
-    console.log(data)
-    throw error;
-  }
-}
-
-export default api;
