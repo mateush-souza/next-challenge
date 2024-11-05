91a982879a451a715dee7c054e56bcb99ac81d56
mateush-souza <mateusguns798@gmail.com>
2024-11-04 21:26:01 -0300

    chore: remove comment

--- src/components/WatsonChat.tsx
+++ src/components/WatsonChat.tsx
@@ -1,18 +1,16 @@
-// components/WatsonChat.tsx
 "use client"
 import { useEffect } from 'react';
 
 interface WatsonAssistantInstance {
   render: () => Promise<void>;
-  // Adicione outros métodos ou propriedades que você deseja usar
 }
 
 const WatsonChat = () => {
   useEffect(() => {
     window.watsonAssistantChatOptions = {
-      integrationID: "b189444e-771b-4db4-8e3d-ca748c534b6f", // O ID da integração.
-      region: "au-syd", // A região onde sua integração está hospedada.
-      serviceInstanceID: "a60974e2-ae9c-4c92-b53f-af031624f3ff", // O ID da sua instância de serviço.
+      integrationID: "b189444e-771b-4db4-8e3d-ca748c534b6f", 
+      region: "au-syd",
+      serviceInstanceID: "a60974e2-ae9c-4c92-b53f-af031624f3ff",
       onLoad: async (instance: WatsonAssistantInstance) => { 
         await instance.render(); 
       }
@@ -24,13 +22,12 @@ const WatsonChat = () => {
 
     document.head.appendChild(script);
 
-    // Limpeza: remover o script se o componente for desmontado
     return () => {
       document.head.removeChild(script);
     };
   }, []);
 
-  return null; // Não renderiza nada, pois é um script de chat
+  return null; 
 };
 
 export default WatsonChat;
